0ca0d8489fbd3e6dbb2fdbb50ee6c957d7e8e29a
ddderi <maxime830@live.fr>
2022-06-22 03:33:22 +0200

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color) VALUES ('Mr. Chocolate', 12, 'M', 'Black');
+INSERT INTO bears (name, age, sex, color) VALUES ('Rowdy', 1, 'F', 'Black');
+INSERT INTO bears (name, age, sex, color) VALUES ('Tabitha', 6, 'M', 'Black');
+INSERT INTO bears (name, age, sex, color) VALUES ('Sergeant Brown', 8, 'F', 'Black');
+INSERT INTO bears (name, age, sex, color) VALUES ('Melissa', 2, 'M', 'Black');
+INSERT INTO bears (name, age, sex, color) VALUES ('Grinch', 2, 'F', 'Black');
+INSERT INTO bears (name, age, sex, color) VALUES ('Wendy', 9, 'F', 'Black');
+INSERT INTO bears (name, age, sex, color) VALUES (NULL, 4, 'M', 'Black');
